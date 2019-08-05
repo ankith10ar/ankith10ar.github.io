3c96864737275ed2bf4fcad9415ad95a37b761d4
Ankith <ankith10reddy@ymail.com>
2019-08-05 23:10:04 +0530

    skills and footer added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var prevScrollpos = window.pageYOffset;
+/*var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
@@ -7,4 +7,72 @@ var currentScrollPos = window.pageYOffset;
     document.getElementById("navbar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
+}*/
+
+/*$('skills-section').appear(function() {
+    move();
+});*/
+
+
+/*$('skills-section').on('inview', function(event, isInView) {
+  if (isInView) {
+    move();
+      console.log(isInView);
+  } else {
+    // element has gone out of viewport
+  }
+});*/
+var counter=0;
+
+
+$(function() {
+       $('#skills-section').waypoint(function() {
+           if (counter==0){
+                move();
+               counter++;
+           }
+         }, {
+           offset: '100%'
+         });
+    });
+
+
+
+function move(){
+    var bar = document.getElementsByClassName("progress");
+//    console.log(bar);
+    
+    var iniVal = [];
+    for(var a=0;a<bar.length;a++){
+        iniVal[a]=parseInt((bar[a].childNodes[1].style.width).replace('%',''));
+    }
+    
+    /*var iniVal = bar.forEach(function(item,index) {
+        iniVal = parseInt(item.childNodes[0].style.width.replace('%',''));
+    });*/
+//    console.log(iniVal);
+    
+    var dup = iniVal.slice();
+    
+    var id = setInterval(frame, 10);
+    
+    function frame() {
+    var count=0;
+    for (var a=0;a<iniVal.length;a++){
+        if(iniVal[a]--<0){
+            count++;
+        }else{
+            bar[a].childNodes[1].style.width = (dup[a]-iniVal[a])+'%';
+//            console.log(dup[a]-iniVal[a]);
+        }
+        if(count==iniVal.length)
+            clearInterval(id);
+    }
+        
+    /*iniVal.forEach(function(item,index){
+        
+    })   */     
+  }
 }
+
+
